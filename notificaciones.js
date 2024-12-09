724b9434b69bc26b8a82d858ef5f1dc743b8313d
adrianvidalr <adrianvidalr@gmail.com>
2024-12-09 19:12:25 +0100

    Archivos iniciales

---
+++ notificaciones.js
@@ -0,0 +1,9 @@
+// notificaciones.js
+// Este archivo manejará las notificaciones para las tareas
+
+function enviarNotificacion(mensaje) {
+    console.log("Notificación: " + mensaje);
+}
+
+// Ejemplo de uso
+enviarNotificacion("Tarea a punto de vencer.");
